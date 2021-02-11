d5b5edae29dda5a82bfd17d92b189354de1f30ba
Nezhdanov Sergei <tsebom@yandex.ru>
2021-02-11 23:28:44 +0400

    update homework2

--- src/ru/geekbrains/javaCore1/Homework2.java
+++ src/ru/geekbrains/javaCore1/Homework2.java
@@ -11,19 +11,19 @@ public class Homework2 {
 
     public static void main(String[] args) {
         System.out.println("Exercise 1: ");
-        exercise1();
+        reversBit();
         System.out.println("");
         System.out.println("Exercise 2: ");
-        exercise2();
+        fillArrayByStepThree();
         System.out.println("");
         System.out.println("Exercise 3: ");
-        exercise3();
+        expandByTwo();
         System.out.println("");
         System.out.println("Exercise 4: ");
-        exercise4();
+        createSquareArray();
         System.out.println("");
         System.out.println("Exercise 5: ");
-        exercise5();
+        searchMaxAndMin();
         System.out.println("");
         System.out.println("Exercise 6: ");
         System.out.println(isEqualParts(new int[]{2, 2, 2, 1, 2, 2, 10, 1}));
@@ -31,18 +31,18 @@ public class Homework2 {
         System.out.println(isEqualParts(new int[]{1, 1, 1, 2, 10}));
         System.out.println("");
         System.out.println("Exercise 7: ");
-        move(new int[]{1, 2, 3}, 1);
-        move(new int[]{1, 2, 3}, 7);
-        move(new int[]{3, 5, 6, 1}, -2);
-        move(new int[]{3, 5, 6, 1}, -10);
+        moveArray(new int[]{1, 2, 3}, 1);
+        moveArray(new int[]{1, 2, 3}, 7);
+        moveArray(new int[]{3, 5, 6, 1}, -2);
+        moveArray(new int[]{3, 5, 6, 1}, -10);
         System.out.println("");
     }
 
     /* 1. Задать целочисленный массив, состоящий из элементов 0 и 1. Например: [ 1, 1, 0, 0, 1, 0, 1, 1, 0, 0 ].
     *     С помощью цикла и условия заменить 0 на 1, 1 на 0
     * */
-    public static void exercise1() {
-        int[] array = new int[]{1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
+    public static void reversBit() {
+        int[] array = {1, 1, 0, 0, 1, 0, 1, 1, 0, 0};
         System.out.println(Arrays.toString(array));
 
         for (int i = 0; i < array.length; i++) {
@@ -59,7 +59,7 @@ public class Homework2 {
 
     /* 2. Задать пустой целочисленный массив размером 8. С помощью цикла заполнить его значениями 0 3 6 9 12 15 18 21
     * */
-    public static void exercise2() {
+    public static void fillArrayByStepThree() {
         int[] array = new int[8];
         int value = 0;
         for (int i = 0; i < 8; i ++) {
@@ -72,8 +72,8 @@ public class Homework2 {
 
     /* 3. Задать массив [ 1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 ] пройти по нему циклом, и числа меньшие 6 умножить на 2
     * */
-    public static void exercise3() {
-        int[] array = new int[]{1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
+    public static void expandByTwo() {
+        int[] array = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         System.out.println(Arrays.toString(array));
 
         for (int i = 0; i < array.length; i++) {
@@ -90,10 +90,10 @@ public class Homework2 {
     *     если обе сложно). Определить элементы одной из диагоналей можно по следующему принципу: индексы таких
     *     элементов равны, то есть [0][0], [1][1], [2][2], …, [n][n]
     * */
-    public static void exercise4() {
+    public static void createSquareArray() {
         int[][] array = new int[10][10];
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
                 if (j == i || i + j == 10 -1) {
                     array[i][j] = 1;
                 }
@@ -103,8 +103,8 @@ public class Homework2 {
             }
         }
 
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
                 System.out.print(array[i][j] + " ");
             }
             System.out.println("");
@@ -113,8 +113,8 @@ public class Homework2 {
 
     /* 5. Задать одномерный массив и найти в нем минимальный и максимальный элементы (без помощи интернета)
     * */
-    public static void exercise5() {
-        int[] array = new int[]{3, 1, 2, 5, 4, 6, 7, 9, 8};
+    public static void searchMaxAndMin() {
+        int[] array = {3, 1, 2, 5, 4, 6, 7, 9, 8};
         int min = array[0];
         int max = array[0];
         for (int i = 1; i < array.length; i++) {
@@ -155,7 +155,7 @@ public class Homework2 {
     *     Примеры: [ 1, 2, 3 ] при n = 1 (на один вправо) -> [ 3, 1, 2 ]; [ 3, 5, 6, 1]
     *     при n = -2 (на два влево) -> [ 6, 1, 3, 5 ]. При каком n в какую сторону сдвиг можете выбирать сами
     * */
-    public static void move(int[] array, int n) {
+    public static void moveArray(int[] array, int n) {
         System.out.print(Arrays.toString(array) + " -> ");
         int k = Math.abs(n);//number of offsets
         //this condition  makes less number of offsets
